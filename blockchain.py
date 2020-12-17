18e6a5770859324c6789a658953bced345373c46
Ramon-Goveia <ramonalangoveia@hotmail.com>
2020-12-17 13:15:42 -0300

    Imports classe blockchain e bloco gênesis

--- blockchain.py
+++ blockchain.py
@@ -5,3 +5,14 @@ Created on Thu Dec 17 12:53:11 2020
 @author: Ramon
 """
 
+import datetime
+import hashlib
+import json
+from flask import flask, jsonify
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = '0')
+        
+        
